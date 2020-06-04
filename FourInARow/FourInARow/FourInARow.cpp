4b6cc0493e8815f8390b361ca04f09ab0da5105b
Bennik2000 <bennik.ko@gmail.com>
2020-06-04 17:22:20 +0200

    Add "Hello World" application with a statically linked library

---
+++ FourInARow/FourInARow/FourInARow.cpp
@@ -0,0 +1,5 @@
+#include <FourInARowLib.h>
+
+int main() {
+    printHelloWorld();
+}
---
+++ FourInARow/FourInARowLib/FourInARowLib.cpp
@@ -0,0 +1,12 @@
+// FourInARowLib.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
+//
+
+#include "pch.h"
+
+#include "FourInARowLib.h"
+#include <iostream>
+#include <ostream>
+
+void printHelloWorld() {
+    std::cout << "Hello World" << std::endl;
+}
