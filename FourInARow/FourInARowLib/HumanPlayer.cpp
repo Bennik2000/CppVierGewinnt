61f48e8a0c1b61ed8e92f63ab4d1efce1eac345c
CodingMarco <codingmarco@gmail.com>
2020-06-23 23:50:21 -0700

    Added HumanPlayer class

---
+++ FourInARow/FourInARowLib/HumanPlayer.cpp
@@ -0,0 +1,15 @@
+#include "pch.h"
+#include "HumanPlayer.h"
+#include "GameRules.h"
+#include "Ui.h"
+
+HumanPlayer::HumanPlayer(const std::string &name, const TeamEnum &team, std::shared_ptr<Ui> ui)
+        : Player(name, team, ui)
+{
+}
+
+void HumanPlayer::play(std::shared_ptr<GameRules> gameRules)
+{
+    int column = ui->readValidColumn(gameRules);
+    gameRules->placeToken(column);
+}
