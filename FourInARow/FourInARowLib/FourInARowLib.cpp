8951187ebc663ee35e7c86d849a78acdc291452d
CodingMarco <codingmarco@gmail.com>
2020-07-09 02:42:09 -0700

    Disable precompiled headers for the entire solution because of conflicts with GTest

--- FourInARow/FourInARowLib/FourInARowLib.cpp
+++ FourInARow/FourInARowLib/FourInARowLib.cpp
@@ -1,8 +1,6 @@
 // FourInARowLib.cpp : Hiermit werden die Funktionen für die statische Bibliothek definiert.
 //
 
-#include "pch.h"
-
 #include "FourInARowLib.h"
 #include <iostream>
 #include <ostream>
--- FourInARow/FourInARowLib/GameBoard.cpp
+++ FourInARow/FourInARowLib/GameBoard.cpp
@@ -1,5 +1,3 @@
-#include "pch.h"
-
 #include "GameBoard.h"
 
 using std::vector;
